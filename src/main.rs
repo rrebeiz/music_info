9203ffac416eb5852b131f5272ceff77bb84b212
intersys <intersys@pop-os.speedport.ip>
2026-04-22 09:57:21 +0200

    updated retry mechanism, should no longer crash when no player is detected

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
+use music_info::{get_metadata, notify};
+use std::thread::sleep;
+use std::time::Duration;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-        let mut last_song: Option<String> = None;
+    let mut last_song: Option<String> = None;
     loop {
+        let player = get_metadata();
+        match player {
+            Ok(player) => {
+                if Some(&player.playing) != last_song.as_ref() {
+                    notify(&player)?;
+                    last_song = Some(player.playing);
+                }
+            }
 
-        let player = music_info::get_metadata()?;
-        // println!("{:#?}", player);
-        if Some(&player.playing) != last_song.as_ref() {
-        music_info::notify(&player)?;
-        last_song = Some(player.playing);
+            Err(_) => {
+                sleep(Duration::from_secs(2));
+            }
         }
-    std::thread::sleep(std::time::Duration::from_secs(2));
+        // println!("{:#?}", player);
+        sleep(Duration::from_secs(2));
     }
 }
